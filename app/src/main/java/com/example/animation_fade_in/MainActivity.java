eb8203cdc8612c9a8f5e6c52dae9813ebe90744c
HOUDA LALAOUI MOUTAOUAKKIL <houdalalaoui115@gmail.com>
2023-04-15 02:30:56 +0000

    animation fade in

---
+++ app/src/main/java/com/example/animation_fade_in/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.animation_fade_in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+TextView text;
+Animation anim;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       text=(TextView) findViewById(R.id.text);
+       anim= AnimationUtils.loadAnimation(this,R.anim.fade_in);
+       text.setAnimation(anim);
+    }
+}
